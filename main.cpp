e09366503bb2d3a7733597180b58e6d09b46768f
S4977690 <91629502+S4977690@users.noreply.github.com>
2021-12-01 20:06:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     int n=0;
     cin >> n;
-    if(numeriprimi(n,n/2)){
+    if(numeriprimi(n,n/2)==true){
         cout << "numero primo" << endl;
     } else {
         cout << "numero non primo" << endl;
     }
 
     return 0;
+}
